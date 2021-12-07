84935c29c618cc06d16349bfa41a9e3a8f869da7
kennosuke0707 <kd1306640@st.kobedenshi.ac.jp>
2021-12-07 10:11:19 +0900

    Co-authored-by: nima-ouchi <nima-ouchi@users.noreply.github.com> Co-authored-by: KasaiYuuto <KasaiYuuto@users.noreply.github.com>

---
+++ login.php
@@ -0,0 +1,8 @@
+<form method="POST" action="./login_db.php">
+    <table>
+        <tr><td>Eメール</td><td><input type="text" name="userId" required></td></tr>
+        <tr><td>パスワード</td><td><input type="password" name="password" required></td></tr>
+        <tr><td colspan="2"><input type="submit" value="ログイン"></td></tr>
+    </table>
+</form>
+<a href="./signup.php"><span class="button_image">新規登録はこちらから</span></a>
