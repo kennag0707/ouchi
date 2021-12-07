115a168651b214c80486e9e5d38c6beb4ee003a6
kennosuke0707 <kd1306640@st.kobedenshi.ac.jp>
2021-12-07 11:31:07 +0900

    main

--- login.php
+++
@@ -1,8 +0,0 @@
-<form method="POST" action="./login_db.php">
-    <table>
-        <tr><td>Eメール</td><td><input type="text" name="userId" required></td></tr>
-        <tr><td>パスワード</td><td><input type="password" name="password" required></td></tr>
-        <tr><td colspan="2"><input type="submit" value="ログイン"></td></tr>
-    </table>
-</form>
-<a href="./signup.php"><span class="button_image">新規登録はこちらから</span></a>
